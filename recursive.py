294a770f2656f4a337868de65785185bdd860349
burakcinardeu <124098194+burakcinardeu@users.noreply.github.com>
2023-02-14 15:07:55 +0100

    added input support

--- recursive.py
+++ recursive.py
@@ -1,12 +1,18 @@
-x = 0
-previous = 0
-current = 1
-while x<10:
+num_for_fib=int(input("write a number for fibonacci series="))
+x=0
+y=1
+for i in range(num_for_fib):
+    z=x+y
+    x=y
+    y=z
+    print(z)
     
-    new = current + previous
-    print(new)
-    previous = current
-    current = new
-    x = x+1
-print(new)
+    
+
+
+
+
+
+
+
 
